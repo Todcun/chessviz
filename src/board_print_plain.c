03cec7d767566a68bda4e4ab7ff28261c4ee74f2
Tod <stas.wolf15@gmail.com>
2019-04-14 21:33:50 +0700

    add code

---
+++ src/board_print_plain.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "board.h"
+#include "board_print.h"
+#include "board_print_plain.h"
+#include "input.h"
+void printDeck(char deck[8][8]) 
+{
+	for (int n = 1; n < 9; ++n)
+	{
+		printf ("%d ", n);
+		for (int s = 1; s < 9; ++s) 
+		{
+			printf("%c ", deck[n][s]);
+
+		}
+		printf("\n");
+	}
+	printf("  a b c d e f g h");
+	printf("\n\n");  
+}
---
+++ src/main.c
@@ -0,0 +1,16 @@
+#include "board.h"
+#include "board_print.h"
+#include "board_print_plain.h"
+#include "input.h"
+int main()
+{
+	char deck[8][8];
+	board_print(deck);
+	while (1)
+	{
+		printDeck(deck);
+		check(deck, 0);
+	}	
+	return 0;
+
+}
